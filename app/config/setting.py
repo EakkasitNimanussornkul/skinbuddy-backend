56ac055561a47d350c2178938eee919f82b71ef9
Eakkasit Nimanussornkul <eakkasitnim@gmail.com>
2026-05-09 20:26:33 +0700

    refactor(auth): added line channel in setting to read from env

--- app/config/setting.py
+++ app/config/setting.py
@@ -1,16 +1,23 @@
-# Loads environment variables (DB URL, Secret Keys)
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    # --- Database & Supabase ---
     DATABASE_URL: SecretStr
-    
     SUPABASE_URL: str
-    SUPABASE_KEY: str  
+    SUPABASE_ANON_KEY: str  
+    SUPABASE_SERVICE_ROLE_KEY: str 
+    SUPABASE_JWT_SECRET: str 
 
+    # --- LINE Login ---
+    # We will use 'CHANNEL' to match your console
+    LINE_CHANNEL_ID: str           
+    LINE_CHANNEL_SECRET: str       
+    LINE_REDIRECT_URI: str         
+
+    # --- Frontend ---
     FRONTEND_URL: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-# Create the globally available settings object
 settings = Settings()
